cd737083a10015b1a5e8cb2f8fc49bc439cf053c
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-10-23 00:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+  const a = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return a === a.split("").reverse().join("");
 }
-module.exports = palindrome
+
+module.exports = palindrome;
